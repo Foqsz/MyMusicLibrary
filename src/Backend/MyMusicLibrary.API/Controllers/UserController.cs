adaef0b5f4db12664a972c35f5a8a1fc9f5a8a0c
Foqsz <contatovictorvinicius05@gmail.com>
2025-07-28 12:15:08 -0300

    feat: adding user inicial

---
+++ src/Backend/MyMusicLibrary.API/Controllers/UserController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyMusicLibrary.Communication.Request;
+using MyMusicLibrary.Communication.Responses;
+
+namespace MyMusicLibrary.API.Controllers;
+[Route("api/[controller]")]
+[ApiController]
+public class UserController : ControllerBase
+{
+    [HttpPost]
+    [ProducesResponseType(typeof(ResponseRegisteredUserJson), StatusCodes.Status201Created)]
+    public IActionResult Register(RequestRegisterUserJson request)
+    {
+        return Created();
+    }
+}
---
+++ src/Shared/MyMusicLibrary.Communication/Responses/ResponseRegisteredUserJson.cs
@@ -0,0 +1,5 @@
+﻿namespace MyMusicLibrary.Communication.Responses;
+public class ResponseRegisteredUserJson
+{
+    public string Name { get; set; } = string.Empty;
+}
