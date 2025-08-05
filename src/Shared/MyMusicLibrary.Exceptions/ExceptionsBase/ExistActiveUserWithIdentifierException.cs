2058605693848abf3d2657a64784d1574790de32
Foqsz <contatovictorvinicius05@gmail.com>
2025-08-05 09:58:33 -0300

    feat: news exceptions

---
+++ src/Shared/MyMusicLibrary.Exceptions/ExceptionsBase/ExistActiveUserWithIdentifierException.cs
@@ -0,0 +1,11 @@
+﻿using System.Net;
+
+namespace MyMusicLibrary.Exceptions.ExceptionsBase;
+public class ExistActiveUserWithIdentifierException : MyMusicLibraryException
+{
+    public ExistActiveUserWithIdentifierException(string message) : base(message)
+    {
+    }
+    public override IList<string> GetErrorMessages() => [Message];
+    public override HttpStatusCode GetStatusCode() => HttpStatusCode.Unauthorized;
+}
