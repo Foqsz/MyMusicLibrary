51dae411dd8b171f0319aab031ea2dd7ae4be144
Foqsz <contatovictorvinicius05@gmail.com>
2025-08-05 09:56:56 -0300

    feat: migration

---
+++ src/Backend/MyMusicLibrary.Infrastructure/DataAccess/Migrations/Versions/Version02.cs
@@ -0,0 +1,17 @@
+﻿using FluentMigrator;
+using MyMusicLibrary.Infrastructure.DataAccess.Migrations;
+
+namespace MyMusicLibrary.Infrastructure.Migrations.Versions;
+
+[Migration(DatabaseVersions.TABLE_MUSIC, "Create table to save the music's songs informations")]
+public class Version0000002 : VersionBase
+{
+    public override void Up()
+    {
+        CreateTable("Music")
+            .WithColumn("Name").AsString(255).NotNullable()
+            .WithColumn("Artist").AsString(100).NotNullable()
+            .WithColumn("Album").AsString(100).NotNullable()
+            .WithColumn("UserId").AsInt64().NotNullable().ForeignKey("FK_User_Music_Id", "Users", "Id");
+    }
+}
