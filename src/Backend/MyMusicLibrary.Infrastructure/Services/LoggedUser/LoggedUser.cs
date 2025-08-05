25b653b7ef47abfca47fedf68ec6a67d9363d082
Foqsz <contatovictorvinicius05@gmail.com>
2025-08-05 09:58:11 -0300

    feat: codigo para pegar o usuario logado antes de adicionar a musica

---
+++ src/Backend/MyMusicLibrary.Infrastructure/Services/LoggedUser/LoggedUser.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyMusicLibrary.Domain.Entities;
+using MyMusicLibrary.Domain.Security.Tokens;
+using MyMusicLibrary.Domain.Services.LoggedUser;
+using MyMusicLibrary.Infrastructure.DataAccess;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+
+namespace MyMusicLibrary.Infrastructure.Services.LoggedUser;
+public class LoggedUser : ILoggedUser
+{
+    private readonly MyMusicLibraryDbContext _dbContext;
+    private readonly ITokenProvider _token;
+
+    public LoggedUser(MyMusicLibraryDbContext dbContext, ITokenProvider token)
+    {
+        _dbContext = dbContext;
+        _token = token;
+    }
+
+    public async Task<User> User()
+    {
+        var token = _token.Value();
+
+        var tokenHandler = new JwtSecurityTokenHandler();
+
+        var jwtSecurityToken = tokenHandler.ReadJwtToken(token);
+
+        var identifier = jwtSecurityToken.Claims.First(c => c.Type == ClaimTypes.Sid).Value;
+
+        var userIdentifier = Guid.Parse(identifier);
+
+        return await _dbContext
+            .Users
+            .AsNoTracking()
+            .FirstAsync(user => user.Active && user.UserIdentifier == userIdentifier);
+    }
+}
