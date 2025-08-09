78877c7c0ec4c3e853ef91690824ffa315f3465c
Foqsz <contatovictorvinicius05@gmail.com>
2025-08-09 16:47:40 -0300

    fix: ajuste no builder.

---
+++ tests/CommonTestUtilities/Repositores/MusicReadOnlyRepositoryBuilder.cs
@@ -0,0 +1,27 @@
+﻿using Moq;
+using MyMusicLibrary.Domain.Entities;
+using MyMusicLibrary.Domain.Repositories.Music;
+
+namespace CommonTestUtilities.Repositores;
+public class MusicReadOnlyRepositoryBuilder
+{
+    private readonly Mock<IMusicReadOnlyRepository> _repository;
+
+    public MusicReadOnlyRepositoryBuilder() => _repository = new Mock<IMusicReadOnlyRepository>();
+
+    public MusicReadOnlyRepositoryBuilder GetById(User user, Music? music)
+    {
+        if (music is not null)
+        {
+            _repository.Setup(repository => repository.GetById(user, music.Id))
+                .ReturnsAsync(music);
+
+        }
+        return this;
+    }
+
+    public IMusicReadOnlyRepository Build()
+    {
+        return _repository.Object;
+    }
+}
