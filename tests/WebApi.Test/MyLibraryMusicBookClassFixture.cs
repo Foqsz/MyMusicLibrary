bba238f482f7e3fbef3fd2dc02d7f35de95cd815
Foqsz <contatovictorvinicius05@gmail.com>
2025-08-14 10:40:10 -0300

    feat: testes de integração user delete accoumt.

--- tests/WebApi.Test/MyLibraryMusicBookClassFixture.cs
+++ tests/WebApi.Test/MyLibraryMusicBookClassFixture.cs
@@ -30,6 +30,14 @@ public class MyLibraryMusicBookClassFixture : IClassFixture<CustomWebApplication
         return await _httpClient.GetAsync(method);
     }
 
+    protected async Task<HttpResponseMessage> DoPut(string method, object request, string token, string culture = "en")
+    {
+        ChangeRequestCulture(culture);
+        AuthorizeRequest(token);
+
+        return await _httpClient.PutAsJsonAsync(method, request);
+    }
+
     private void AuthorizeRequest(string token)
     {
         if (string.IsNullOrWhiteSpace(token))
---
+++ tests/WebApi.Test/User/Delete/DeleteUserAccountTest.cs
@@ -0,0 +1,68 @@
+﻿using CommonTestUtilities.Requests;
+using CommonTestUtilities.Tokens.Generator;
+using Microsoft.AspNetCore.Mvc.Testing;
+using MyMusicLibrary.Infrastructure.Security.Tokens.Access.Generator;
+using Shouldly;
+using System.Net;
+using System.Text.Json;
+using Xunit;
+
+namespace WebApi.Test.User.Delete;
+public class DeleteUserAccountTest : MyLibraryMusicBookClassFixture
+{
+    private readonly string method = "user/update";
+
+    private readonly Guid _userIdentifier;
+
+    public DeleteUserAccountTest(CustomWebApplicationFactory factory) : base(factory)
+    {
+        _userIdentifier = factory.GetUserIdentifier();
+    }
+
+    [Fact]
+    public async Task Success()
+    {
+        var user = RequestUpdateJsonBuilder.Build();
+
+        var token = JwtTokenGeneratorBuilder.Build().Generate(_userIdentifier);
+
+        var userUpdate = await DoPut(method: method, user, token);
+        userUpdate.StatusCode.ShouldBe(HttpStatusCode.OK); 
+    }
+
+    [Fact]
+    public async Task Error_Name_Empty()
+    {
+        var user = RequestUpdateJsonBuilder.Build();
+        user.Name = string.Empty;
+
+        var token = JwtTokenGeneratorBuilder.Build().Generate(_userIdentifier);
+
+        var userUpdate = await DoPut(method: method, user, token);
+        userUpdate.StatusCode.ShouldBe(HttpStatusCode.BadRequest);
+    }
+
+    [Fact]
+    public async Task Error_Email_Empty()
+    {
+        var user = RequestUpdateJsonBuilder.Build();
+        user.Email = string.Empty;
+
+        var token = JwtTokenGeneratorBuilder.Build().Generate(_userIdentifier);
+
+        var userUpdate = await DoPut(method: method, user, token);
+        userUpdate.StatusCode.ShouldBe(HttpStatusCode.BadRequest);
+    }
+
+    [Fact]
+    public async Task Error_Email_Invalid()
+    {
+        var user = RequestUpdateJsonBuilder.Build();
+        user.Email = "monza.com";
+
+        var token = JwtTokenGeneratorBuilder.Build().Generate(_userIdentifier);
+
+        var userUpdate = await DoPut(method: method, user, token);
+        userUpdate.StatusCode.ShouldBe(HttpStatusCode.BadRequest);
+    }
+}
