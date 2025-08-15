78246edd6b2726397497ed4f9b229593195bb71f
Foqsz <contatovictorvinicius05@gmail.com>
2025-08-15 11:55:59 -0300

    feat: validator

---
+++ src/Backend/MyMusicLibrary.Application/UseCases/User/ChangePassword/UserChangePasswordValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+using MyMusicLibrary.Application.SharedValidators;
+using MyMusicLibrary.Communication.Request;
+
+namespace MyMusicLibrary.Application.UseCases.User.ChangePassword;
+public class UserChangePasswordValidator : AbstractValidator<RequestUserChangePassword>
+{
+    public UserChangePasswordValidator()
+    {
+        RuleFor(u => u.NewPassword)
+            .SetValidator(new PasswordValidator<RequestUserChangePassword>());
+    }
+}
