aaf5f68d7b5c741ff9502feeeead71e387064ffb
Foqsz <contatovictorvinicius05@gmail.com>
2025-08-15 11:55:42 -0300

    feat: use cases

---
+++ src/Backend/MyMusicLibrary.Application/UseCases/User/ChangePassword/IUserChangePasswordUseCase.cs
@@ -0,0 +1,7 @@
+﻿using MyMusicLibrary.Communication.Request;
+
+namespace MyMusicLibrary.Application.UseCases.User.ChangePassword;
+public interface IUserChangePasswordUseCase
+{
+    Task Execute(RequestUserChangePassword request);
+}
