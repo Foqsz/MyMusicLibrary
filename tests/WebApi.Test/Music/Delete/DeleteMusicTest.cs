c601262511fdee0db6491f5286f9669c1bd8a85f
Foqsz <contatovictorvinicius05@gmail.com>
2025-08-20 12:32:17 -0300

    feat: testes de integração

---
+++ tests/WebApi.Test/Music/Delete/DeleteMusicTest.cs
@@ -0,0 +1,41 @@
+﻿using CommonTestUtilities.Entities;
+using CommonTestUtilities.Tokens.Generator;
+using Shouldly;
+using System.Net;
+using Xunit;
+
+namespace WebApi.Test.Music.Delete;
+public class DeleteMusicTest : MyLibraryMusicBookClassFixture
+{
+    private readonly string method = "music";
+    private readonly Guid _userIdentifier;
+
+    public DeleteMusicTest(CustomWebApplicationFactory factory) : base(factory)
+    {
+        _userIdentifier = factory.GetUserIdentifier();
+    }
+
+    [Fact]
+    public async Task Success()
+    {
+        (var user, var _) = UserBuilder.Build();
+        var token = JwtTokenGeneratorBuilder.Build().Generate(_userIdentifier);
+
+        var music = MusicBuilder.Build(user);
+
+        var musicDelete = await DoDelete($"{method}/{music.Id}", token);
+
+        musicDelete.StatusCode.ShouldBe(HttpStatusCode.NoContent);
+    }
+
+    [Fact]
+    public async Task Error_Music_Invalid_Id()
+    {
+        (var user, var _) = UserBuilder.Build();
+        var token = JwtTokenGeneratorBuilder.Build().Generate(_userIdentifier); 
+
+        var musicDelete = await DoDelete($"{method}/{2}", token);
+
+        musicDelete.StatusCode.ShouldBe(HttpStatusCode.BadRequest);
+    }
+}
---
+++ tests/WebApi.Test/Music/Register/RegisterMusicTest.cs
@@ -0,0 +1,30 @@
+﻿using CommonTestUtilities.Entities;
+using CommonTestUtilities.Requests;
+using CommonTestUtilities.Tokens.Generator;
+using Shouldly;
+using System.Net;
+using Xunit;
+
+namespace WebApi.Test.Music.Register;
+public class RegisterMusicTest : MyLibraryMusicBookClassFixture
+{
+    private readonly string method = "music";
+    private readonly Guid _userIdentifier;
+
+    public RegisterMusicTest(CustomWebApplicationFactory factory) : base(factory)
+    {
+        _userIdentifier = factory.GetUserIdentifier();
+    }
+
+    [Fact]
+    public async Task Success()
+    {
+        var token = JwtTokenGeneratorBuilder.Build().Generate(_userIdentifier);
+
+        var request = RequestMusicJsonBuilder.Build();
+
+        var musicRegister = await DoPost(method, request, token);
+
+        musicRegister.StatusCode.ShouldBe(HttpStatusCode.Created);
+    }
+}
