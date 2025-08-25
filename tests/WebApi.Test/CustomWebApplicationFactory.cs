aec8a01d3ffff0574099e817601fb99395d900ed
Foqsz <contatovictorvinicius05@gmail.com>
2025-08-25 12:51:36 -0300

    feat: mock db

--- tests/WebApi.Test/CustomWebApplicationFactory.cs
+++ tests/WebApi.Test/CustomWebApplicationFactory.cs
@@ -44,6 +44,7 @@ public class CustomWebApplicationFactory : WebApplicationFactory<Program>
     public string GetEmail() => _user.Email;
     public string GetPassword() => _password;
     public string GetName() => _user.Name;
+    public string GetMusicName() => _music.Name;    
     public Guid GetUserIdentifier() => _user.UserIdentifier; 
 
     private void StartDataBase(MyMusicLibraryDbContext dbContext)
