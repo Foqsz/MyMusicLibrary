f729e2930a2b056c96bbd3d0f5b44d011f23fd8d
Foqsz <contatovictorvinicius05@gmail.com>
2025-08-25 12:48:18 -0300

    feat: repository

--- src/Backend/MyMusicLibrary.Domain/Repositories/Music/IMusicReadOnlyRepository.cs
+++ src/Backend/MyMusicLibrary.Domain/Repositories/Music/IMusicReadOnlyRepository.cs
@@ -4,4 +4,5 @@ public interface IMusicReadOnlyRepository
     Task<Entities.Music?> GetById(Entities.User user, long musicId);
     Task<IList<Entities.Music>> GetForDashboard(Entities.User user);
     Task<bool> ThereIsThisSong(Entities.User user, string musicName, string album);
+    Task<IList<Entities.Music>> Search(Entities.User user, string name);
 }
