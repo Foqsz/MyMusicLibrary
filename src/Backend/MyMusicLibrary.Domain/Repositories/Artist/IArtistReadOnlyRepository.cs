3e43cbf8944be181d8cfa565d09051e6c0be3859
Foqsz <contatovictorvinicius05@gmail.com>
2025-08-26 11:39:59 -0300

    feat: repositorios

---
+++ src/Backend/MyMusicLibrary.Domain/Repositories/Artist/IArtistReadOnlyRepository.cs
@@ -0,0 +1,5 @@
+﻿namespace MyMusicLibrary.Domain.Repositories.Artist;
+public interface IArtistReadOnlyRepository
+{
+    Task<IList<Entities.Artist>> SearchArtist(Entities.User user, string name);
+}
