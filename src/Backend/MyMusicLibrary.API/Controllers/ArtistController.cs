407e2b646d55536d6aec58767e41cbdc27dd0cb4
Foqsz <contatovictorvinicius05@gmail.com>
2025-08-26 11:34:34 -0300

    feat: controller

---
+++ src/Backend/MyMusicLibrary.API/Controllers/ArtistController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyMusicLibrary.API.Attributes;
+using MyMusicLibrary.Application.UseCases.Artist;
+using MyMusicLibrary.Communication.Responses;
+
+namespace MyMusicLibrary.API.Controllers;
+[Route("[controller]")]
+[ApiController]
+[AuthenticatedUser]
+public class ArtistController : ControllerBase
+{
+    [HttpGet]
+    [Route("search")]
+    [ProducesResponseType(typeof(ResponseProfileArtistJson), StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    public async Task<IActionResult> SearchArtist([FromServices] ISearchArtistUseCase useCase, string name)
+    {
+        var result = await useCase.Execute(name); 
+
+        if(result.Count == 0)
+            return BadRequest();
+
+        return Ok(result);
+    }
+}
