4b926ec6789fcb00e7a23e4c6a7c57b0a842136f
Foqsz <contatovictorvinicius05@gmail.com>
2025-08-26 11:40:26 -0300

    fea: new responses

---
+++ src/Shared/MyMusicLibrary.Communication/Responses/ResponseProfileArtistJson.cs
@@ -0,0 +1,9 @@
+﻿using MyMusicLibrary.Domain.Dtos;
+
+namespace MyMusicLibrary.Communication.Responses;
+public class ResponseProfileArtistJson
+{
+    public string Name { get; set; } = string.Empty;
+    public string Genre { get; set; } = string.Empty;
+    public IList<MusicDto> Music { get; set; } = null!;
+}
---
+++ src/Shared/MyMusicLibrary.Communication/Responses/ResponseProfileMusicJson.cs
@@ -0,0 +1,7 @@
+﻿namespace MyMusicLibrary.Communication.Responses;
+public class ResponseProfileMusicJson
+{
+    public string Name { get; set; } = string.Empty;
+    public string Album { get; set; } = string.Empty;
+    public string Artist { get; set; } = string.Empty;
+}
