088dccc9e3075dedaa3ee6542e8d41cd2ef0f76b
Foqsz <contatovictorvinicius05@gmail.com>
2025-08-29 10:59:08 -0300

    feat: repository

---
+++ src/Backend/MyMusicLibrary.Domain/Repositories/Playlist/IPlaylistWriteOnlyRepository.cs
@@ -0,0 +1,5 @@
+﻿namespace MyMusicLibrary.Domain.Repositories.Playlist;
+public interface IPlaylistWriteOnlyRepository
+{
+    Task Create(Entities.User user, Entities.Playlist request);
+}
---
+++ src/Backend/MyMusicLibrary.Infrastructure/DataAccess/Repositories/Playlist/PlaylistWriteOnlyRepository.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyMusicLibrary.Domain.Extensions;
+using MyMusicLibrary.Domain.Repositories.Playlist;
+
+namespace MyMusicLibrary.Infrastructure.DataAccess.Repositories.Playlist;
+public class PlaylistWriteOnlyRepository : IPlaylistWriteOnlyRepository
+{
+    private readonly MyMusicLibraryDbContext _dbContext;
+
+    public PlaylistWriteOnlyRepository(MyMusicLibraryDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    public async Task Create(Domain.Entities.User user, Domain.Entities.Playlist request) => await _dbContext.Playlist.AddAsync(request);
+}
