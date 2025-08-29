a059ff2f5eb107a21530e6b650c28319a6034728
Foqsz <contatovictorvinicius05@gmail.com>
2025-08-29 10:58:56 -0300

    feat: validator

---
+++ src/Backend/MyMusicLibrary.Application/UseCases/Playlist/Create/CreatePlaylistValidator.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+using MyMusicLibrary.Communication.Request;
+using MyMusicLibrary.Exceptions;
+
+namespace MyMusicLibrary.Application.UseCases.Playlist.Create;
+public class CreatePlaylistValidator : AbstractValidator<RequestCreatePlaylistJson>
+{
+    public CreatePlaylistValidator()
+    {
+        RuleFor(p => p.Name)
+            .NotEmpty()
+            .WithMessage(ResourceMessagesException.PLAYLIST_NAME_EMPTY)
+            .MaximumLength(100)
+            .WithMessage(ResourceMessagesException.PLAYLIST_NAME_TOO_LONG);
+
+        RuleFor(p => p.Description)
+            .MaximumLength(500)
+            .WithMessage(ResourceMessagesException.PLAYLIST_DESCRIPTION_TOO_LONG);
+    }
+}
