f31990c4caae1c96973ca6d730146102c2ee4924
Foqsz <contatovictorvinicius05@gmail.com>
2025-08-29 10:54:16 -0300

    feat: migration para adicionar playlist ao banco de dados.

---
+++ src/Backend/MyMusicLibrary.Infrastructure/DataAccess/Migrations/Versions/Version09.cs
@@ -0,0 +1,19 @@
+﻿using FluentMigrator;
+using MyMusicLibrary.Infrastructure.DataAccess.Migrations;
+
+namespace MyMusicLibrary.Infrastructure.Migrations.Versions
+{
+    [Migration(DatabaseVersions.TABLE_PLAYLIST, "Create playlist")]
+    public class Version0000009 : VersionBase
+    {
+        public override void Up()
+        {
+            CreateTable("Playlist")
+                .WithColumn("Name").AsString(100).NotNullable()
+                .WithColumn("Description").AsString(1024).NotNullable()
+                .WithColumn("UserId").AsInt64().NotNullable()
+                    .ForeignKey("FK_Playlist_User_Id", "Users", "Id")
+                    .OnDelete(System.Data.Rule.Cascade);
+        }
+    }
+}
