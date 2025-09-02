b41d6388d5f268eab947afb8b920d0e9e2699e0d
Foqsz <contatovictorvinicius05@gmail.com>
2025-09-02 12:43:34 -0300

    feat: testes de integração

---
+++ tests/WebApi.Test/Playlist/Update/UpdatePlaylistTest.cs
@@ -0,0 +1,70 @@
+﻿using CommonTestUtilities.Requests;
+using CommonTestUtilities.Tokens.Generator;
+using MyMusicLibrary.Exceptions;
+using Shouldly;
+using Xunit;
+
+namespace WebApi.Test.Playlist.Update;
+public class UpdatePlaylistTest : MyLibraryMusicBookClassFixture
+{
+    private const string method = "playlist";
+    private readonly Guid _userIdentifier;
+    private string _playlistName;
+    private string _playlistDescription;
+    private long _playlistId;
+
+    public UpdatePlaylistTest(CustomWebApplicationFactory factory) : base(factory)
+    {
+        _userIdentifier = factory.GetUserIdentifier();
+        _playlistName = factory.GetPlaylistName();
+        _playlistDescription = factory.GetPlaylistDescription();
+        _playlistId = factory.GetPlaylistId();
+    }
+
+    [Fact]
+    public async Task Success()
+    {
+        var token = JwtTokenGeneratorBuilder.Build().Generate(userIdentifier: _userIdentifier);
+
+        var newPlaylist = RequestFromPlaylistJsonBuilder.Build();
+
+        var response = await DoPut($"{method}/{_playlistId}", newPlaylist, token);
+
+        response.EnsureSuccessStatusCode();
+        response.StatusCode.ShouldBe(System.Net.HttpStatusCode.OK);
+    }
+
+    [Fact]
+    public async Task Error_Dados_NoChanges()
+    {
+        var token = JwtTokenGeneratorBuilder.Build().Generate(userIdentifier: _userIdentifier);
+
+        var newPlaylist = RequestFromPlaylistJsonBuilder.Build();
+        newPlaylist.Name = _playlistName;
+        newPlaylist.Description = _playlistDescription;
+
+        var response = await DoPut($"{method}/{_playlistId}", newPlaylist, token);
+
+        response.StatusCode.ShouldBe(System.Net.HttpStatusCode.BadRequest);
+
+        var content = await response.Content.ReadAsStringAsync();
+        content.ShouldContain(ResourceMessagesException.UPDATE_ERROR);
+    }
+
+    [Fact]
+    public async Task Error_Id_Playlist_NotFound()
+    {
+        var token = JwtTokenGeneratorBuilder.Build().Generate(userIdentifier: _userIdentifier);
+
+        var newPlaylist = RequestFromPlaylistJsonBuilder.Build();
+
+        var createPlaylist = await DoPost("playlist/create", newPlaylist, token);
+
+        var response = await DoPut($"{method}/{5}", newPlaylist, token);
+
+        response.StatusCode.ShouldBe(System.Net.HttpStatusCode.NotFound);
+
+        var content = await response.Content.ReadAsStringAsync();
+        content.ShouldContain(ResourceMessagesException.PLAYLIST_NOTFOUND);
+    }
+}
