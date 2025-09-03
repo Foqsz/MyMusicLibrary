2572ec206331017cb89dcc1d1440fba1d2fc7d2e
Foqsz <contatovictorvinicius05@gmail.com>
2025-09-03 11:43:05 -0300

    feat: get por Id useCases

---
+++ src/Backend/MyMusicLibrary.Application/UseCases/Playlist/GetPlaylistId/IGetPlaylistIdUseCase.cs
@@ -0,0 +1,7 @@
+﻿using MyMusicLibrary.Communication.Responses;
+
+namespace MyMusicLibrary.Application.UseCases.Playlist.GetPlaylistId;
+public interface IGetPlaylistIdUseCase
+{
+    Task<ResponsePlaylistAllJson?> Execute(long id);
+}
