19a6b46ded65799f04d6142c71716f3495ebccd4
Foqsz <contatovictorvinicius05@gmail.com>
2025-09-05 11:54:12 -0300

    feat: repositories

--- src/Backend/MyMusicLibrary.Domain/Repositories/User/IUserReadOnlyRepository.cs
+++ src/Backend/MyMusicLibrary.Domain/Repositories/User/IUserReadOnlyRepository.cs
@@ -1,6 +1,7 @@
 ﻿namespace MyMusicLibrary.Domain.Repositories.User;
 public interface IUserReadOnlyRepository
 {
+    Task<IList<Entities.User>> GetByIds(IEnumerable<long> ids);
     Task<Entities.User> GetById(long id);
     Task<Entities.User?> GetByEmail(string email); 
     Task<bool> ExistActiveUserWithEmail(string email);
