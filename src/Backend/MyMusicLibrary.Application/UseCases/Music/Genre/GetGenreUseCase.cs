4234209d2c6301ed317950690da9e96404c181e0
Foqsz <contatovictorvinicius05@gmail.com>
2025-09-10 11:54:57 -0300

    feat: removendo auto mapper desnecessario

--- src/Backend/MyMusicLibrary.Application/UseCases/Music/Genre/GetGenreUseCase.cs
+++ src/Backend/MyMusicLibrary.Application/UseCases/Music/Genre/GetGenreUseCase.cs
@@ -1,5 +1,4 @@
-﻿using AutoMapper;
-using MyMusicLibrary.Communication.Responses;
+﻿using MyMusicLibrary.Communication.Responses;
 using MyMusicLibrary.Domain.Extensions;
 using MyMusicLibrary.Domain.Repositories.Music;
 using MyMusicLibrary.Exceptions;
@@ -9,12 +8,10 @@ namespace MyMusicLibrary.Application.UseCases.Music.Genre;
 public class GetGenreUseCase : IGetGenreUseCase
 {
     private readonly IMusicReadOnlyRepository _repositoryRead;
-    private readonly IMapper _mapper;
 
-    public GetGenreUseCase(IMusicReadOnlyRepository repositoryRead, IMapper mapper)
+    public GetGenreUseCase(IMusicReadOnlyRepository repositoryRead)
     {
         _repositoryRead = repositoryRead;
-        _mapper = mapper;
     }
 
     public async Task<IList<ResponseGenreJson>> Execute()
