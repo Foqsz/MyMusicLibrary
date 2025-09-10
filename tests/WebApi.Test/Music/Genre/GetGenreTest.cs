b07cbb23d3a899a9efb329704d57a487fded4e6f
Foqsz <contatovictorvinicius05@gmail.com>
2025-09-10 11:55:20 -0300

    feat: testes de integração

---
+++ tests/WebApi.Test/Music/Genre/GetGenreTest.cs
@@ -0,0 +1,26 @@
+﻿using CommonTestUtilities.Tokens.Generator;
+using Shouldly;
+using Xunit;
+
+namespace WebApi.Test.Music.Genre;
+public class GetGenreTest : MyLibraryMusicBookClassFixture
+{
+    private const string method = "music/genre";
+    private readonly Guid _userIdentifier;
+
+    public GetGenreTest(CustomWebApplicationFactory factory) : base(factory)
+    {
+        _userIdentifier = factory.GetUserIdentifier();
+    }
+
+    [Fact]
+    public async Task Success()
+    {
+        var token = JwtTokenGeneratorBuilder.Build().Generate(_userIdentifier);
+
+        var result = await DoGet(method, token);
+
+        result.StatusCode.ShouldBe(System.Net.HttpStatusCode.OK);
+        result.Content.ShouldNotBeNull();
+    }
+}
