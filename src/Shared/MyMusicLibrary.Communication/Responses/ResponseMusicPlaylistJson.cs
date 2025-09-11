f6e6955fcdd2155ae5f460da3af661b0f82f4288
Foqsz <contatovictorvinicius05@gmail.com>
2025-09-11 11:56:10 -0300

    feat: responses e requests

---
+++ src/Shared/MyMusicLibrary.Communication/Responses/ResponseMusicPlaylistJson.cs
@@ -0,0 +1,7 @@
+﻿namespace MyMusicLibrary.Communication.Responses;
+public class ResponseMusicPlaylistJson
+{
+    public string Music { get; set; } = string.Empty;
+    public string Playlist { get; set; } = string.Empty;
+
+}
