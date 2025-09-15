dceaad2324d19f3a77b9c27cb6d34acf034e529b
Foqsz <contatovictorvinicius05@gmail.com>
2025-09-15 10:14:49 -0300

    feat: request builder

---
+++ tests/CommonTestUtilities/Requests/RequestMusicPlaylistJsonBuilder.cs
@@ -0,0 +1,15 @@
+﻿using Bogus;
+using MyMusicLibrary.Communication.Request;
+
+namespace CommonTestUtilities.Requests;
+public class RequestMusicPlaylistJsonBuilder
+{
+    public static RequestMusicPlaylistJson Build()
+    {
+        return new RequestMusicPlaylistJson()
+        {
+            MusicId = 1,
+            PlaylistId = 1,
+        };
+    }
+}
