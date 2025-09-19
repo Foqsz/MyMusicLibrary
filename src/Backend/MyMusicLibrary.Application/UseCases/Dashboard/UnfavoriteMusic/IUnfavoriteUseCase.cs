b1e227a50b28447e6f8c0b465244bccd1c79527a
Foqsz <contatovictorvinicius05@gmail.com>
2025-09-19 11:15:35 -0300

    feat: unfavorite music useCases

---
+++ src/Backend/MyMusicLibrary.Application/UseCases/Dashboard/UnfavoriteMusic/IUnfavoriteUseCase.cs
@@ -0,0 +1,5 @@
+﻿namespace MyMusicLibrary.Application.UseCases.Dashboard.RemoveMusicFavorite;
+public interface IUnfavoriteUseCase
+{
+    Task Execute(long favoriteMusicId);
+}
---
+++ src/Backend/MyMusicLibrary.Application/UseCases/Dashboard/UnfavoriteMusic/UnfavoriteUseCase.cs
@@ -0,0 +1,39 @@
+﻿
+using MyMusicLibrary.Domain.Repositories.Music;
+using MyMusicLibrary.Domain.Repositories.UnitOfWork;
+using MyMusicLibrary.Domain.Services.LoggedUser;
+using MyMusicLibrary.Exceptions;
+using MyMusicLibrary.Exceptions.ExceptionsBase;
+
+namespace MyMusicLibrary.Application.UseCases.Dashboard.RemoveMusicFavorite;
+public class UnfavoriteUseCase : IUnfavoriteUseCase
+{
+    private readonly ILoggedUser _loggedUser;
+    private readonly IMusicReadOnlyRepository _repositoyRead;
+    private readonly IMusicWriteOnlyRepository _repositoryWrite;
+    private readonly IUnitOfWork _unitOfWork;
+
+    public UnfavoriteUseCase(ILoggedUser loggedUser,
+        IMusicReadOnlyRepository repositoyRead,
+        IMusicWriteOnlyRepository repositoryWrite,
+        IUnitOfWork unitOfWork)
+    {
+        _loggedUser = loggedUser;
+        _repositoyRead = repositoyRead;
+        _repositoryWrite = repositoryWrite;
+        _unitOfWork = unitOfWork;
+    }
+
+    public async Task Execute(long favoriteMusicId)
+    {
+        var user = await _loggedUser.User();
+
+        var musicFavorited = _repositoyRead.GetMusicFavoriteId(user, favoriteMusicId);
+
+        if (musicFavorited is null)
+            throw new NotFoundException(ResourceMessagesException.MUSIC_EMPTY);
+
+        await _repositoryWrite.UnfavoriteMusic(favoriteMusicId);
+        await _unitOfWork.Commit();
+    }
+}
