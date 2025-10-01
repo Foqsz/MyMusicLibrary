2f57e97c8b0d137741a7b4f6be467efe5ceeca13
Foqsz <contatovictorvinicius05@gmail.com>
2025-10-01 09:47:21 -0300

    fix: mais ajustes em migrations feitas de forma errada.

--- src/Backend/MyMusicLibrary.Infrastructure/DataAccess/Migrations/Versions/Version06.cs
+++ src/Backend/MyMusicLibrary.Infrastructure/DataAccess/Migrations/Versions/Version06.cs
@@ -8,19 +8,26 @@ public class Version0000006 : VersionBase
 {
     public override void Up()
     {
-        // Cria tabela Music
-        CreateTable("Music")
-            .WithColumn("Name").AsString(255).NotNullable()
-            .WithColumn("Album").AsString(100).NotNullable()
-            .WithColumn("UserId").AsInt64().NotNullable()
-                .ForeignKey("FK_Music_User_Id", "users", "Id")
-                .OnDelete(System.Data.Rule.Cascade);
+        if (!Schema.Table("Music").Exists() && !Schema.Table("music").Exists())
+        {
+            // Cria tabela Music
+            CreateTable("Music")
+                .WithColumn("Name").AsString(255).NotNullable()
+                .WithColumn("Album").AsString(100).NotNullable()
+                .WithColumn("UserId").AsInt64().NotNullable()
+                    .ForeignKey("FK_Music_User_Id", "users", "Id")
+                    .OnDelete(System.Data.Rule.Cascade);
+        }
 
-        // Cria tabela Artist
-        CreateTable("Artist")
-            .WithColumn("Name").AsString(255).NotNullable()
-            .WithColumn("Genre").AsString(100).NotNullable()
-            .WithColumn("MusicId").AsInt64().NotNullable()
-                .ForeignKey("FK_Artist_Music_Id", "music", "Id");
+        if (!Schema.Table("Artist").Exists() && !Schema.Table("artist").Exists())
+        {
+            // Cria tabela Artist
+            CreateTable("Artist")
+                .WithColumn("Name").AsString(255).NotNullable()
+                .WithColumn("Genre").AsString(100).NotNullable()
+                .WithColumn("MusicId").AsInt64().NotNullable()
+                    .ForeignKey("FK_Artist_Music_Id", "music", "Id")
+                    .OnDelete(System.Data.Rule.Cascade);
+        }
     }
 }
