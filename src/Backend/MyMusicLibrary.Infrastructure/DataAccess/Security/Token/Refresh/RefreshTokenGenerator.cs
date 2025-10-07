a54d78f94fec681516ffd4efd8bec21b5c9933f3
Foqsz <contatovictorvinicius05@gmail.com>
2025-10-07 12:13:17 -0300

    feat: refresh token generator

---
+++ src/Backend/MyMusicLibrary.Infrastructure/DataAccess/Security/Token/Refresh/RefreshTokenGenerator.cs
@@ -0,0 +1,7 @@
+﻿using MyMusicLibrary.Domain.Security.Tokens;
+
+namespace MyMusicLibrary.Infrastructure.DataAccess.Security.Token.Refresh;
+public class RefreshTokenGenerator : IRefreshTokenGenerator
+{
+    public string Generate() => Convert.ToBase64String(Guid.NewGuid().ToByteArray());
+}
