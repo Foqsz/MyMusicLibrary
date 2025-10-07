8efc51b20010d7f9a66f383a2efded55ade5b013
Foqsz <contatovictorvinicius05@gmail.com>
2025-10-07 12:09:28 -0300

    feat: use cases para refresh token

---
+++ src/Backend/MyMusicLibrary.Application/UseCases/Token/RefreshToken/UseRefreshTokenUseCase.cs
@@ -0,0 +1,55 @@
+﻿using MyMusicLibrary.Communication.Request;
+using MyMusicLibrary.Communication.Responses;
+using MyMusicLibrary.Domain.Repositories.Token;
+using MyMusicLibrary.Domain.Repositories.UnitOfWork;
+using MyMusicLibrary.Domain.Security.Tokens;
+using MyMusicLibrary.Domain.ValueObjects;
+using MyMusicLibrary.Exceptions.ExceptionsBase;
+
+namespace MyMusicLibrary.Application.UseCases.Token.RefreshToken;
+public class UseRefreshTokenUseCase : IUseRefreshTokenUseCase
+{
+    private readonly ITokenRepository _tokenRepository;
+    private readonly IUnitOfWork _unitOfWork;
+    private readonly IAccessTokenGenerator _accessTokenGenerator;
+    private readonly IRefreshTokenGenerator _refreshTokenGenerator;
+
+    public UseRefreshTokenUseCase(ITokenRepository tokenRepository, 
+        IUnitOfWork unitOfWork, 
+        IAccessTokenGenerator accessTokenGenerator, 
+        IRefreshTokenGenerator refreshTokenGenerator)
+    {
+        _tokenRepository = tokenRepository;
+        _unitOfWork = unitOfWork;
+        _accessTokenGenerator = accessTokenGenerator;
+        _refreshTokenGenerator = refreshTokenGenerator;
+    }
+
+    public async Task<ResponseTokensJson> Execute(RequestNewTokenJson request)
+    {
+        var refreshToken = await _tokenRepository.Get(request.RefreshToken);
+
+        if (refreshToken is null)
+            throw new RefreshTokenNotFoundException();
+
+        var refreshTokenValidUntil = refreshToken.CreatedOn.AddDays(MyMusicLibraryRuleConstants.REFRESH_TOKEN_EXPIRATION);
+        if(DateTime.Compare(refreshTokenValidUntil, DateTime.UtcNow) < 0)
+            throw new RefreshTokenExpiredException();
+
+        var newRefreshToken = new Domain.Entities.RefreshToken()
+        {
+            Value = _refreshTokenGenerator.Generate(),
+            UserId = refreshToken.UserId,
+        };
+
+        await _tokenRepository.SaveNewRefreshToken(newRefreshToken);
+
+        await _unitOfWork.Commit();
+
+        return new ResponseTokensJson()
+        {
+            AccessToken = _accessTokenGenerator.Generate(refreshToken.User.UserIdentifier),
+            RefreshToken = newRefreshToken.Value
+        };
+    }
+}
