a05a852c00146454635a9dca3c2c84f9587924ad
Foqsz <contatovictorvinicius05@gmail.com>
2025-10-07 12:12:32 -0300

    feat: exceptions

---
+++ src/Shared/MyMusicLibrary.Exceptions/ExceptionsBase/RefreshTokenNotFoundException.cs
@@ -0,0 +1,13 @@
+﻿using System.Net;
+
+namespace MyMusicLibrary.Exceptions.ExceptionsBase;
+public class RefreshTokenNotFoundException : MyMusicLibraryException
+{
+    public RefreshTokenNotFoundException() : base(ResourceMessagesException.EXPIRED_SESSION)
+    {
+    }
+
+    public override IList<string> GetErrorMessages() => [Message];
+
+    public override HttpStatusCode GetStatusCode() => HttpStatusCode.Unauthorized;
+}
