2e3e06e2b4d41c7b43298eab47b16d4b0e39f0b7
Foqsz <contatovictorvinicius05@gmail.com>
2025-10-08 10:23:39 -0300

    feat: refresh token builders

---
+++ tests/CommonTestUtilities/Entities/RefreshTokenBuilder.cs
@@ -0,0 +1,16 @@
+﻿using CommonTestUtilities.Tokens.Refresh;
+using MyMusicLibrary.Domain.Entities;
+
+namespace CommonTestUtilities.Entities;
+public class RefreshTokenBuilder
+{
+    public static RefreshToken Build(User user)
+    {
+        return new RefreshToken
+        {
+            Value = RefreshTokenGeneratorBuilder.Build().Generate(),
+            UserId = user.Id,
+            User = user
+        };
+    }
+}
