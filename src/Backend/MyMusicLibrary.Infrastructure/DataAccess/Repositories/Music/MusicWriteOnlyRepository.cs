bbc2e41acdf43ce094b3d92807da75a0026fa4be
Foqsz <contatovictorvinicius05@gmail.com>
2025-10-09 12:45:57 -0300

    feat: correções no delete. Agora está removendo de todos os locais.

--- src/Backend/MyMusicLibrary.Infrastructure/DataAccess/Repositories/Music/MusicWriteOnlyRepository.cs
+++ src/Backend/MyMusicLibrary.Infrastructure/DataAccess/Repositories/Music/MusicWriteOnlyRepository.cs
@@ -1,4 +1,5 @@
-﻿using MyMusicLibrary.Domain.Repositories.Music;
+﻿using Microsoft.EntityFrameworkCore;
+using MyMusicLibrary.Domain.Repositories.Music;
 
 namespace MyMusicLibrary.Infrastructure.DataAccess.Repositories.Music;
 public class MusicWriteOnlyRepository : IMusicWriteOnlyRepository
@@ -18,6 +19,14 @@ public class MusicWriteOnlyRepository : IMusicWriteOnlyRepository
     public async Task Delete(long musicId)
     {
         var music = await _dbContext.Music.FindAsync(musicId);
+        var musicFavorite = await _dbContext.UserFavoritesMusic.Where(m => m.UserId == music!.UserId && m.MusicId == music.Id).FirstOrDefaultAsync();
+        var artist = await _dbContext.Artist.Where(a => a.MusicId == music!.Id).ToListAsync();
+
+        if (musicFavorite is not null)
+            _dbContext.UserFavoritesMusic.Remove(musicFavorite);
+
+        if (artist is not null && artist.Count > 0)
+            _dbContext.Artist.RemoveRange(artist);
 
         _dbContext.Music.Remove(music!);
     }
