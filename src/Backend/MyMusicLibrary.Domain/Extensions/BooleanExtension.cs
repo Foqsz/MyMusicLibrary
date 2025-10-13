75e6df2f6b41daf4750dd99ee75cec7adf0f5637
Foqsz <contatovictorvinicius05@gmail.com>
2025-10-13 11:09:55 -0300

    feat: new boolean extension

--- src/Backend/MyMusicLibrary.Domain/Extensions/BooleanExtension.cs
+++ src/Backend/MyMusicLibrary.Domain/Extensions/BooleanExtension.cs
@@ -2,4 +2,6 @@
 public static class BooleanExtension
 {
     public static bool IsFalse(this bool value) => !value;
+    public static bool IsTrue(this bool value) => value;
+    public static bool NotEmpty(this string? value) => string.IsNullOrWhiteSpace(value).IsFalse();
 }
