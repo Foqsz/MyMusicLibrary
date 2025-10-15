77bea4b887a8af9b969abe423229d8f9a40f5772
Foqsz <contatovictorvinicius05@gmail.com>
2025-10-15 10:09:14 -0300

    feat: ajustes no use case pra receber a url

--- src/Backend/MyMusicLibrary.Application/UseCases/Music/GetById/GetMusicByIdUseCase.cs
+++ src/Backend/MyMusicLibrary.Application/UseCases/Music/GetById/GetMusicByIdUseCase.cs
@@ -2,6 +2,7 @@
 using MyMusicLibrary.Communication.Responses;
 using MyMusicLibrary.Domain.Repositories.Music;
 using MyMusicLibrary.Domain.Services.LoggedUser;
+using MyMusicLibrary.Domain.Services.Storage.Aws;
 using MyMusicLibrary.Exceptions;
 using MyMusicLibrary.Exceptions.ExceptionsBase;
 
@@ -11,12 +12,17 @@ public class GetMusicByIdUseCase : IGetMusicByIdUseCase
     private readonly IMusicReadOnlyRepository _repository;
     private readonly ILoggedUser _logged;
     private readonly IMapper _mapper;
+    private readonly IS3Service _s3Service;
 
-    public GetMusicByIdUseCase(IMusicReadOnlyRepository repository, ILoggedUser logged, IMapper mapper)
+    public GetMusicByIdUseCase(IMusicReadOnlyRepository repository, 
+        ILoggedUser logged, 
+        IMapper mapper, 
+        IS3Service s3Service)
     {
         _repository = repository;
         _logged = logged;
         _mapper = mapper;
+        _s3Service = s3Service;
     }
 
     public async Task<ResponseProfileMusicJson> Execute(long id)
@@ -28,7 +34,11 @@ public class GetMusicByIdUseCase : IGetMusicByIdUseCase
         if (music is null)
             throw new NotFoundException(ResourceMessagesException.MUSIC_EMPTY);
 
+        var musicUrl = await _s3Service.GetFileUrl(music.MusicKey!);
+
         var musicMapper = _mapper.Map<Domain.Entities.Music, ResponseProfileMusicJson>(music);
+
+        musicMapper.UrlMusicS3 = musicUrl.url;
 
         return musicMapper;
     }
