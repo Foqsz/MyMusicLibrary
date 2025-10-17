4fb3007bcff622df673f6cc59c77a9877177618e
Foqsz <contatovictorvinicius05@gmail.com>
2025-10-16 23:54:20 -0300

    fix: namespace

--- src/Backend/MyMusicLibrary.Infrastructure/DataAccess/Migrations/Versions/Version01.cs
+++ src/Backend/MyMusicLibrary.Infrastructure/DataAccess/Migrations/Versions/Version01.cs
@@ -1,7 +1,7 @@
 ﻿using FluentMigrator;
-using MyMusicLibrary.Infrastructure.DataAccess.Migrations;
+using MyMusicLibrary.Infrastructure.Migrations.Versions;
 
-namespace MyMusicLibrary.Infrastructure.Migrations.Versions;
+namespace MyMusicLibrary.Infrastructure.DataAccess.Migrations.Versions;
 
 [Migration(DatabaseVersions.TABLE_USER, "Create table to save the user's information")]
 public class Version0000001 : VersionBase
