75d6b7b4ef1d579abb4cee3dcce358853ae0c7fb
Foqsz <contatovictorvinicius05@gmail.com>
2025-11-03 11:19:35 -0300

    feat: rename Music Use Cases

---
+++ src/Backend/MyMusicLibrary.Application/UseCases/Music/Update/IUpdateMusicUseCase.cs
@@ -0,0 +1,8 @@
+﻿using MyMusicLibrary.Communication.Request;
+using MyMusicLibrary.Communication.Responses;
+
+namespace MyMusicLibrary.Application.UseCases.Music.Update;
+public interface IUpdateMusicUseCase
+{
+    Task<ResponseProfileMusicJson> Execute(RequestUpdateMusic request);
+}
